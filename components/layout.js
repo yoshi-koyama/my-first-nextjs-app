94bf95e21062c34b0ad77d2d1c4e07f03fd7ed5f
yoshi-koyama <koyama@reytech.co.jp>
2021-04-26 01:08:09 +0900

    finished all the course of tutorial

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Yoshihito Koyama'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'My Dev Log'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Brush up what I learn from Paiza Skill Check Test"
         />
         <meta
           property="og:image"
@@ -42,7 +42,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/profile.png"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,12 +25,30 @@ export default function Home() {
           Hey Guys! I am a Japanese software developer.</p>
         <p>
           I work for educating software developers in Myanmar.
-          <br/>
-          I have been creating Japanese leaning resources on <a href="https://www.facebook.com/reytechmyanmar" rel="noopener noreferrer">Facebook</a>.
+        <br />
+          I have been creating Japanese leaning resources on <a href="https://www.facebook.com/reytechmyanmar"
+            rel="noopener noreferrer">Facebook</a>.
         </p>
         <p>
-          Just contact me on{' '}<a href="https://www.facebook.com/yoshihito.koyama.73" rel="noopener noreferrer">my Facebook page</a>.
+          Just contact me on{' '}<a href="https://www.facebook.com/yoshihito.koyama.73" rel="noopener noreferrer">my
+          Facebook page</a>.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
